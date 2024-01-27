5677f9585ddb9ca0660e5f1fc0cd8d653165fedd
Tomas.Lawton <tomas.lawton@accenture.com>
2024-01-27 12:41:26 +1100

    Refctor env

---
+++ src/environment.js
@@ -0,0 +1,179 @@
+import * as THREE from "three";
+import { generateRadialGradient } from "./gradient.js";
+
+export const environment = (() => {
+  class World {
+    constructor(params) {
+      this.scene = params.scene;
+    }
+
+    create() {
+      if (this.scene) {
+        this.createWorld();
+        this.createRings();
+        this.createRunway();
+        this.addLights();
+        this.addParticles();
+      }
+    }
+
+    createWorld() {
+      this.scene.fog = new THREE.FogExp2(0x111111, 0.08);
+
+      const cubeTextureLoader = new THREE.CubeTextureLoader();
+      const textureUrls = [
+        'public/blue/bkg1_left.png',
+        'public/blue/bkg1_right.png',
+        'public/blue/bkg1_top.png',
+        'public/blue/bkg1_bot.png',
+        'public/blue/bkg1_front.png',
+        'public/blue/bkg1_back.png'
+      ];
+      const cubeMap = cubeTextureLoader.load(textureUrls);
+      this.scene.background = cubeMap;
+
+      const groundGeometry = new THREE.PlaneGeometry(35, 150, 35, 35);
+      groundGeometry.rotateX(-Math.PI / 2);
+      const groundMaterial = new THREE.MeshStandardMaterial({
+        color: 0x111111,
+        metalness: 0.8,
+        roughness: 0.5,
+        side: THREE.DoubleSide,
+      });
+      const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+      groundMesh.castShadow = false;
+      groundMesh.receiveShadow = true;
+      this.scene.add(groundMesh);
+    }
+    createRings() {
+      const glowGeometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+      const glowMaterial = new THREE.MeshStandardMaterial({
+        emissive: 0xff6600, 
+        emissiveIntensity: 5,
+        color: 0x777777,
+      });
+    
+      const generateRingPoints = (radius, pointCount, h) => {
+        for (let i = 0; i < pointCount; i++) {
+          const angle = (Math.PI * 2 * i) / pointCount;
+          const x = radius * Math.cos(angle);
+          const y = radius * Math.sin(angle);
+          const glowPoint = new THREE.Mesh(glowGeometry, glowMaterial);
+          glowPoint.position.set(x, 0, y);
+          this.scene.add(glowPoint);
+        }
+      };
+    
+      let numRings = 5;
+      let r = 4;
+      for (let i = 0; i < numRings; i++) {
+        generateRingPoints(i * r, i * 10, i);
+      }
+    }
+    createRunway() {
+      const glowGeometry2 = new THREE.BoxGeometry(.3, .3, .3);
+      const glowMaterial2 = new THREE.MeshStandardMaterial({
+        emissive: 0xffffff,
+        emissiveIntensity: 3,
+        color: 0x111111,
+      });
+    
+      const runwayLights = (yOff) => {
+        const glowMesh1 = new THREE.Mesh(glowGeometry2, glowMaterial2);
+        const glowMesh2 = new THREE.Mesh(glowGeometry2, glowMaterial2);
+        const glowMesh3 = new THREE.Mesh(glowGeometry2, glowMaterial2);
+        const glowMesh4 = new THREE.Mesh(glowGeometry2, glowMaterial2);
+        glowMesh1.position.set(2, 0, -2 + yOff);
+        glowMesh2.position.set(-2, 0, -2 + yOff);
+        glowMesh3.position.set(2, 0, 2 + yOff);
+        glowMesh4.position.set(-2, 0, 2 + yOff);
+        const glowMeshArray = [glowMesh1, glowMesh2, glowMesh3, glowMesh4];
+        this.scene.add(...glowMeshArray);
+      };
+    
+      let numSquares = 5;
+      for (let i = 0; i < numSquares; i++) {
+        runwayLights(i * 8);
+      }
+    }
+    
+    addLights() {
+      const ambientLight = new THREE.AmbientLight(0x777777, 0.8);
+      this.scene.add(ambientLight);
+
+      const spotLight = new THREE.SpotLight(0xff6600, 2, 30, 1, 1);
+      spotLight.position.set(0, 15, 0);
+      spotLight.castShadow = true;
+      spotLight.shadow.bias = -0.0001;
+      this.scene.add(spotLight);
+
+      const spotLight2 = new THREE.SpotLight(0x00aaff, 2, 30, 1, 1);
+      spotLight2.position.set(0, 15, -15);
+      spotLight2.castShadow = true;
+      spotLight2.shadow.bias = -0.0001;
+      this.scene.add(spotLight2);
+    }
+
+    addParticles() {
+      const particleCount = 120;
+      this.particles = new THREE.Group();
+      this.scene.add(this.particles);
+      const particleGeometry = new THREE.SphereGeometry(0.03, 6, 6);
+
+      const particleMaterial = new THREE.MeshBasicMaterial({
+        color: 0x00aaff,
+        emissive: 0x00aaff,
+        emissiveIntensity: 0.8,
+        side: THREE.DoubleSide,
+        transparent: true,
+        opacity: 0.2,
+      });
+
+      for (let i = 0; i < particleCount; i++) {
+        const particle = new THREE.Mesh(particleGeometry, particleMaterial);
+        particle.position.set(
+          Math.random() * 20 - 10,
+          Math.random() * 10,
+          Math.random() * 20 - 10
+        );
+        this.particles.add(particle);
+
+        this.animateParticle(particle);
+      }
+    }
+
+    animateParticle(particle) {
+      const animationSpeed = 0.0006;
+      const initialPosition = particle.position.clone();
+      particle.userData.animationOffset = Math.random() * Math.PI * 2;
+      particle.onBeforeRender = () => {
+        const time =
+          performance.now() * animationSpeed + particle.userData.animationOffset;
+        particle.position.x = initialPosition.x + Math.sin(time) * 1;
+        particle.position.y = initialPosition.y + Math.sin(time * 2) * 0.5;
+        particle.position.z = initialPosition.z + Math.sin(time * 3) * 1;
+      };
+      this.animateFlicker(particle.material);
+    }
+
+    animateFlicker(material) {
+      const animationSpeed = 0.002;
+      let time = 0;
+
+      function animate() {
+        time += animationSpeed;
+
+        if (material.emissive) {
+          material.emissive.setScalar(Math.abs(Math.sin(time)));
+        }
+
+        requestAnimationFrame(animate);
+      }
+      animate();
+    }
+  }
+
+  return {
+    World: World,
+  };
+})();
--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@ import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 
 import { GUI } from "dat.gui";
-import { generateRadialGradient } from "./gradient.js";
 import { ThirdPersonCamera } from './third-person-view.js'
 import { player_input } from './player-input.js'
 import { entity } from './entity.js'
-
+import { environment } from './environment.js'
 
 // Setup
 
@@ -24,15 +23,8 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(
-//   45,
-//   window.innerWidth / window.innerHeight,
-//   1,
-//   1000
-// );
-// camera.position.set(0, 5, 13);
 const camera = new THREE.PerspectiveCamera(
-  60, // Wider field of view
+  50, 
   window.innerWidth / window.innerHeight,
   1,
   1000
@@ -51,187 +43,9 @@ const composer = new EffectComposer(renderer);
 composer.addPass(renderScene);
 composer.addPass(bloomPass);
 
-// Scene
-
-scene.fog = new THREE.FogExp2(0x111111, 0.08);
-
-const ambientLight = new THREE.AmbientLight(0x777777, 0.8);
-scene.add(ambientLight);
-
-const particleCount = 100;
-const particles = new THREE.Group();
-const particleGeometry = new THREE.SphereGeometry(0.01, 4, 4);
-
-const particleMaterial = new THREE.MeshBasicMaterial({
-  color: 0x00aaff,
-  emissive: 0x00aaff,
-  emissiveIntensity: 0.5,
-  side: THREE.DoubleSide,
-  transparent: true,
-  opacity: 0.4,
-});
-
-for (let i = 0; i < particleCount; i++) {
-  const particle = new THREE.Mesh(particleGeometry, particleMaterial);
-  particle.position.set(
-    Math.random() * 20 - 10,
-    Math.random() * 10,
-    Math.random() * 20 - 10
-  );
-  particles.add(particle);
-
-  animateParticle(particle);
-}
-
-scene.add(particles);
-
-const glowGeometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-const glowMaterial = new THREE.MeshStandardMaterial({
-  emissive: 0xff6600,
-  emissiveIntensity: 2,
-  color: 0x111111,
-});
-const glowGeometry2 = new THREE.BoxGeometry(.3, .3, .3);
-const glowMaterial2 = new THREE.MeshStandardMaterial({
-  emissive: 0xff6600, 
-  emissiveIntensity: 5,
-  color: 0x777777,
-});
-
-
-const spotLight = new THREE.SpotLight(0xff6600, 2, 30, 1, 1); // Orange color for a dystopian feel
-spotLight.position.set(0, 15, 0);
-spotLight.castShadow = true;
-spotLight.shadow.bias = -0.0001;
-scene.add(spotLight);
-
-const spotLight2 = new THREE.SpotLight(0x00aaff, 2, 30, 1, 1); // Blue color for contrast
-spotLight2.position.set(0, 15, -15); // Adjust the Z position
-spotLight2.castShadow = true;
-spotLight2.shadow.bias = -0.0001;
-scene.add(spotLight2);
-
-
-
-
-function generateRingPoints(radius, pointCount, h) {
-  for (let i = 0; i < pointCount; i++) {
-    const angle = (Math.PI * 2 * i) / pointCount;
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle);
-    const glowPoint = new THREE.Mesh(glowGeometry, glowMaterial);
-    glowPoint.position.set(x, 0, y);
-    scene.add(glowPoint);
-  }
-}
-
-let numRings = 5
-let r = 4
-for (let i=0; i<numRings; i++) {
-  generateRingPoints(i*r, i*10, i);
-}
-
-function runwayLights (yOff) {
-  const glowMesh1 = new THREE.Mesh(glowGeometry2, glowMaterial2);
-  const glowMesh2 = new THREE.Mesh(glowGeometry2, glowMaterial2);
-  const glowMesh3 = new THREE.Mesh(glowGeometry2, glowMaterial2);
-  const glowMesh4 = new THREE.Mesh(glowGeometry2, glowMaterial2);
-  glowMesh1.position.set(2, 0, -2 + yOff);
-  glowMesh2.position.set(-2, 0, -2 + yOff);
-  glowMesh3.position.set(2, 0, 2 + yOff);
-  glowMesh4.position.set(-2, 0, 2 + yOff);
-  const glowMeshArray = [glowMesh1, glowMesh2, glowMesh3, glowMesh4];
-  scene.add(...glowMeshArray);
-}
-let numSquares = 5;
-for (let i=0; i<numSquares; i++) {
-  runwayLights(i*10);
-}
-
-
-
-
-
-
-// Add flickering effect to the glow
-animateFlicker(glowMaterial);
-
-function animateParticle(particle) {
-  const animationSpeed = 0.0006;
-  const initialPosition = particle.position.clone();
-
-  particle.userData.animationOffset = Math.random() * Math.PI * 2;
-
-  particle.onBeforeRender = (
-    renderer,
-    scene,
-    camera,
-    geometry,
-    material,
-    group
-  ) => {
-    const time =
-      performance.now() * animationSpeed + particle.userData.animationOffset;
-    particle.position.x = initialPosition.x + Math.sin(time) * 1;
-    particle.position.y = initialPosition.y + Math.sin(time * 2) * 0.5;
-    particle.position.z = initialPosition.z + Math.sin(time * 3) * 1;
-  };
-}
-
-function animateFlicker(material) {
-  const animationSpeed = 0.002;
-  let time = 0;
-
-  function animate() {
-    time += animationSpeed;
-
-    if (material.emissive) {
-      material.emissive.setScalar(Math.abs(Math.sin(time)));
-    }
-
-    requestAnimationFrame(animate);
-  }
-
-  animate();
-}
-
-
-
-
-
-
-
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const textureUrls = [
-  'public/blue/bkg1_left.png', // URL for the negative x-axis (left)
-  'public/blue/bkg1_right.png', // URL for the positive x-axis (right)
-  'public/blue/bkg1_top.png', // URL for the positive y-axis (top)
-  'public/blue/bkg1_bot.png', // URL for the negative y-axis (bottom)
-  'public/blue/bkg1_front.png', // URL for the positive z-axis (front)
-  'public/blue/bkg1_back.png'  // URL for the negative z-axis (back)
-];
-const cubeMap = cubeTextureLoader.load(textureUrls);
-scene.background = cubeMap;
-
-// const gradientTexture = new THREE.Texture(generateRadialGradient());
-// scene.background = gradientTexture;
-
-const groundGeometry = new THREE.PlaneGeometry(35, 150, 35, 35);
-groundGeometry.rotateX(-Math.PI / 2);
-const groundMaterial = new THREE.MeshStandardMaterial({
-  color: 0x111111,
-  metalness: 0.8,
-  roughness: 0.5,
-  side: THREE.DoubleSide,
-});
-const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
-groundMesh.castShadow = false;
-groundMesh.receiveShadow = true;
-scene.add(groundMesh);
-
-
-
-
+// Environment
+const world = new environment.World({ scene: scene });
+world.create()
 
 
 // GUI
@@ -319,7 +133,7 @@ function updateSpaceshipPosition() {
 
 
 
-const playerInputComponent = new player_input.PlayerInput({});
+const playerInputComponent = new player_input.PlayerInput();
 const playerEntity = new entity.Entity();
 playerEntity.AddComponent(playerInputComponent);
 playerEntity.InitEntity();
@@ -357,6 +171,7 @@ loader.load(
     });
 
     tempObject.rotation.y = 1.5 * Math.PI ;
+    // tempObject.position.z += tempObject.scale.z / 2;
     mesh = rotationWrapper;
     updateSpaceshipPosition();
     rotationWrapper.add(tempObject);
@@ -421,7 +236,7 @@ function animate() {
       const rotationAngle = -input.axis1Side * 0.05; 
       mesh.rotation.y += rotationAngle;
 
-      console.log(mesh.position)
+      // console.log(mesh.position)
       controls.update();
       thirdPersonCamera.update();
     }
